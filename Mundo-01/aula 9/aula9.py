4999220fc8297e45865a76e08cb002f629ffefb6
Erick Kamizato Scilo <erick.scilo@omie.com.br>
2024-10-21 08:51:15 -0300

    Create aula9.py

---
+++ Mundo-01/aula 9/aula9.py
@@ -0,0 +1,23 @@
+#Trabalhando com strings
+
+frase = 'Curso em Video Python'
+print('\n')
+
+#Fatiamento
+# [x:y:z]
+# x começo, y final, z pula caracteres 
+
+print(frase[2])
+print(frase[2:13]) # 2 até o 13, excluindo o 13
+print(frase[9:21]) #imprime 'Video Python'
+print(frase[9:21:2]) #pulando de 2 em 2
+print(frase[:5]) # caracter 0 até 4
+print(frase[15:]) # indicando o início mas não indicando o final
+print(frase[9::3])
+print('-'*13)
+
+#Análise
+print(len(frase)) # mostra total de caracteres
+print(frase.count('o')) # conta quantas vezes aparece a letra 'o'
+print(frase.count('o',0,13)) # conta com fatiamento 0 até 13, excluindo o 13
+print(frase.find('deo')) # quantas vezes encontrou 'deo', exibe a posição que começou
