3141fa1d2461d550cb46f2bf794d3978c01039ee
Erick Kamizato Scilo <erick.scilo@omie.com.br>
2024-10-29 09:37:21 -0300

    att exercícios

--- Mundo-01/aula 9/desafio24.py
+++ Mundo-01/aula 9/desafio24.py
@@ -1,6 +1,6 @@
 # Crie um programa que leia o nome de uma cidade e diga se ela começa ou não com o nome "Santos"
 
-cidade = input(('Digite o nome de uma cidade: '))
+cidade = input(('Digite o nome de uma cidade: ')).strip()
 dividido = cidade.split()
 
 if('Santo' in dividido[0].capitalize()):
