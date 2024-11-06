722525a17e51e96fc3c6a076862c3fe4b022cdaa
Erick Kamizato Scilo <erick.scilo@omie.com.br>
2024-11-06 09:50:11 -0300

    Update desafio68.py

--- Mundo-02/aula 15/desafio68.py
+++ Mundo-02/aula 15/desafio68.py
@@ -3,6 +3,7 @@
 # quando o jogador PERDER, mostrando o total de vítorias
 # consecutivas que ele conquistou no final do jogo.
 import random
+print('\n')
 print('=-'*20)
 print('VAMOS JOGAR PAR OU ÍMPAR')
 
@@ -11,7 +12,7 @@ while True:
     print('=-'*20)
     computador = random.randint(0,10)
     usuario = int(input('Diga um valor: '))
-    escolha = str(input('Par ou ímpar?[P/I] ')).upper().strip()
+    escolha = str(input('Par ou ímpar?[P/I] ')).upper().strip()[0]
     
     print(f'Você jogou {usuario} e o computador {computador}. Total de {usuario+computador}')
     print('-'*20)
